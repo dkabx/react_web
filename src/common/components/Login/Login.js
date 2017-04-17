0fede69f90ca57c7649b21edabfbe4995c218626
Sai Ram <sairam@SaiRams-MacBook-Pro.local>
2017-04-17 17:29:09 +0530

    for gitmerge purpose

--- src/common/components/Login/Login.js
+++ src/common/components/Login/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, Modal} from 'react-materialize';
 import Login from '../../../containers/Login/Login';
 
+//for git commit purpose...
 class SigninModal extends Component {
 
     render() {
